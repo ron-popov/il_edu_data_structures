2da56111b019417f50c1809d8983ad9f55bb537a
ron-popov <ron.popov@hotmail.com>
2018-02-28 20:26:34 +0200

    List node CNode.cs implementation

---
+++ Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerScienceTest
+{
+    class Node<T>
+    {
+        private T value;
+        private Node<T> nextNode;
+
+        public Node(T x)
+        {
+            this.value = x;
+            this.nextNode = null;
+        }
+
+        public Node(T x, Node<T> y)
+        {
+            this.value = x;
+            this.nextNode = y;
+        }
+
+        public T GetValue()
+        {
+            return this.value;
+        }
+
+        public Node<T> GetNext()
+        {
+            return this.nextNode;
+        }
+
+        public bool HasNext()
+        {
+            return this.nextNode != null;
+        }
+
+        public void SetValue(T x)
+        {
+            this.value = x;
+        }
+
+        public void SetNext(Node<T> y)
+        {
+            this.nextNode = y;
+        }
+        
+		public override string ToString()
+        {
+            return ("Value : " + this.value + " has next node : " + (this.nextNode == null));
+        }
+    }
+}
